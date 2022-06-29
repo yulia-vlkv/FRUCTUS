49c286a4fe31e4ad811fbcb92562cea7044b70bf
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-06-29 09:51:50 +0200

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Iuliia Volkova on 29.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Iuliia Volkova on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
